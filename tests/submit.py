56f7ac594f62671ed615ef0fd2b73698a1063a0c
Helge Mathee <helge.mathee@fabricengine.com>
2015-11-10 22:35:20 +0100

    restructuring of modules

--- tests/submit.py
+++ tests/submit.py
@@ -1,20 +1,20 @@
-from zookeeper import *
+import zookeeper.zkDB as zkDB
 
-conn = zkConnection()
+conn = zkDB.zkConnection()
 conn.setDebug(True)
-bracket = zkBracket()
+bracket = zkDB.zkBracket()
 
-p2 = zkProject.createNew(conn)
+p2 = zkDB.zkProject.createNew(conn)
 p2.name = 'asdad'
 p2.dcc = 'softimage'
 p2.renderer = 'redshift'
 bracket.push(p2)
 
-i1 = zkInput.createNew(conn)
+i1 = zkDB.zkInput.createNew(conn)
 i1.path = 'c:/temp/test.png'
 bracket.push(i1)
 
-j1 = zkJob.createNew(conn)
+j1 = zkDB.zkJob.createNew(conn)
 j1.project = p2
 j1.input = i1
 j1.user = 'helge'
@@ -22,12 +22,12 @@ j1.type = 'preview'
 bracket.push(j1)
 
 for i in range(1, 10):
-  f = zkFrame.createNew(conn)
+  f = zkDB.zkFrame.createNew(conn)
   f.job = j1
   f.time = i
   bracket.push(f)
 
-  o = zkOutput.createNew(conn)
+  o = zkDB.zkOutput.createNew(conn)
   o.frame = f
   o.path = "//domain/test/test/%d.png" % i
   bracket.push(o)
